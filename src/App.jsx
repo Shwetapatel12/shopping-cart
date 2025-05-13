d339e22a2c425dc23e043cc3434e3a72f10962f1
shweta_P <shwetapatel99585@gmail.com>
2025-05-13 21:34:23 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
-import Products from "./component/Product/Products";
+import Products from "./components/Product/Products";
 import Register from "./Pages/Register";
 import AllProducts from "./Pages/AllProducts";
 import Cart from "./Pages/Cart";
-import { Login } from "@mui/icons-material";
-import Layout from "./component/layout/Layout";
+import Login from "./Pages/Login";
+import Layout from "./components/layout/Layout";
+import ShowProduct from "./Pages/ShowProduct";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<AllProducts />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/allproducts" element={<AllProducts />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ShowProduct />} />
       </Routes>
     </Layout>
   );
